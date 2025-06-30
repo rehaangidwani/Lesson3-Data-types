262dfb12149582afff2c08041ca57878073463f2
rehaangidwani <r3208795@gmail.com>
2025-06-30 15:03:23 +0200

    data types,type casting,reverse string

---
+++ Data type.py
@@ -0,0 +1,15 @@
+name="rehaan"
+print("the value of name variable is ",name,"and the data type of it is",type(name))
+age=14
+print("the value of age variable is ",age,"and the data type of it is",type(age))
+weight=58.5
+print("the value of weight variable is ",weight,"and the data type of it is",type(weight))
+isrespectful=True
+print("the value of isrespectful variable is ",isrespectful,"and the data type of it is",type(isrespectful))
+isrespectful=str(isrespectful)
+print("the value of isrespectful variable is ",isrespectful,"and the data type of it is",type(isrespectful))
+weight=int(weight)
+print("the value of weight variable is ",weight,"and the data type of it is",type(weight))
+age=float(age)
+print("the value of age variable is ",age,"and the data type of it is",type(age))
+print(name[::-1])
